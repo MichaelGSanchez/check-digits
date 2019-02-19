9c083cde1e571d98546723f9aa62874eec4988f2
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-02-19 14:20:43 -0700

    Added test method

---
+++ test/edu/cnm/deepdive/CheckTest.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheckTest {
+
+  @Test
+  void digitSum() {
+    assertEquals(4046,14);
+    assertEquals(6325,16);
+    assertEquals(9788,32);
+    assertEquals(5676,24);
+    assertEquals(88,16);
+    assertEquals(8925,24);
+    assertEquals(2025,9);
+    assertEquals(4738,22);
+    assertEquals(453,12);
+  }
+}
